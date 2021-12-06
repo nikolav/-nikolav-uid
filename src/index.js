78b83922164aaabfe497a76bb9a5da66b0bb77dc
nikolav <admin@nikolav.rs>
2021-12-06 11:27:23 +0000

    adds Date.now to uid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const idcache_ = {};
+const configDefault = { prefix: `#${Date.now()}-` };
 let uid_ = 0;
 
-module.exports = uid;
-
-function uid(config = { prefix: "#" }) {
+const uid = (config = configDefault) => {
   let id_;
   for (; idcache_.hasOwnProperty((id_ = `${config.prefix}${uid_++}`)); );
   return id_;
-}
+};
+
+module.exports = uid;
